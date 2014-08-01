ffd08acb39c89d76a4bcd29868059b4829076c84
Paavan Shanbhag <pshanbhag@platform9.net>
2014-08-01 16:25:01 -0700

    Fix pf9 user setting for ubuntu + remove active role version setting from du_ctl
    Testing done: Ran after-install.sh on Ubuntu and du_ctl script in the DU.
    Observed that they behave fine.
    
    Reviewed by: osoriano (https://rbcommons.com/s/platform9/r/845/)
    
    Issues addressed: IAAS-692, IAAS-780

--- bbone/bbslave/support/debian/after-install.sh
+++ bbone/bbslave/support/debian/after-install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 # Create the pf9 user and group
-id pf9 &>/dev/null || useradd pf9
 grep ^pf9group: /etc/group &>/dev/null || groupadd pf9group
+id pf9 &>/dev/null || useradd -g pf9group -d / -s /usr/sbin/nologin -c "Platform9 user" pf9
+# In cases where pf9 user exists but is not part of the pf9group, explicitly
+# add them
 usermod -aG pf9group pf9
 # Add root also to the pf9group
 usermod -aG pf9group root
