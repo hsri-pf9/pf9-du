020c9fe2bed482180dda53114e24e3de9dbaaa93
osoriano <osoriano@platform9.net>
2014-11-14 17:30:05 -0800

    Fix default role_settings for hosts authorized before Beta1
    Issues addressed: IAAS-1294
    
    Testing done:
      * Upgraded host software for a setup with 2 hosts
        * One host was both a hypervisor and imagelibrary
        * The other host was only a hypervisor
        * The setup had discovered VMs and VMs created from the UI
      * Ensured the rpms were actually updated on the host
      * Successfully launched another VM from the UI after upgrading
        hosts

--- resmgr/resmgr/alembic/versions/2_add_role_settings_column_to_host_table.py
+++ resmgr/resmgr/alembic/versions/2_add_role_settings_column_to_host_table.py
@@ -10,20 +10,31 @@ Create Date: 2014-10-26 16:29:50.428966
 revision = '2'
 down_revision = '1'
 
+import json
 from alembic import op
 from sqlalchemy import Column, String
 
+default_role_settings = json.dumps({
+    "pf9-ostackhost": {
+        "instances_path": "/opt/pf9/data/instances"
+    },
+    "pf9-imagelibrary": {
+        "data_directory": "/var/opt/pf9/imagelibrary/data"
+    }
+})
 
 def upgrade():
     op.add_column(
         'hosts',
-        Column('role_settings', String(2048), default="{}")
+        Column('role_settings', String(2048),
+               server_default=default_role_settings)
     )
     op.add_column(
         'roles',
-        Column('customizable_settings', String(2048), default="{}")
+        Column('customizable_settings', String(2048))
     )
 
 def downgrade():
     op.drop_column('hosts', 'role_settings')
+    op.drop_column('roles', 'customizable_settings')
 
