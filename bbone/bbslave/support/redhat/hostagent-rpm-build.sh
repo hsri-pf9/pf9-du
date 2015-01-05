71d2713f560a11333419d55445b484a9cced1ce6
amrishkapoor <akapoor@platform9.com>
2015-01-05 14:09:52 -0800

    Changes to allow hostagent to send information about hypervisor type

--- bbone/bbslave/support/redhat/hostagent-rpm-build.sh
+++ bbone/bbslave/support/redhat/hostagent-rpm-build.sh
@@ -15,11 +15,12 @@ SPEC_FILE_DIR=`mktemp -d -t pf9-XXX`
 
 # Remove the .spec, the hostagent-rpmbuild.sh file from the BUILD_DIR
 mv $TARBALL_EXPANDED_LOCATION/hostagent.spec $SPEC_FILE_DIR/hostagent.spec
-cp $TARBALL_EXPANDED_LOCATION/hostagent-rpm-build.sh $SPEC_FILE_DIR/hostagent-rpm-build.sh
+mv $TARBALL_EXPANDED_LOCATION/hostagent-rpm-build.sh $SPEC_FILE_DIR/hostagent-rpm-build.sh
 
 sed -i -e "s/CHANGE_TO_YOUR_BROKER_IP/$HOST_IP/" $TARBALL_EXPANDED_LOCATION/etc/pf9/hostagent.conf
 sed -i -e "s/CHANGE_TO_HYPERVISOR_TYPE/$HYPERVISOR_TYPE/" $TARBALL_EXPANDED_LOCATION/etc/pf9/hostagent.conf
 
 rpmbuild -bb --define "_topdir $RPMBUILD_DIR" --define "_src_dir $TARBALL_EXPANDED_LOCATION"  --define "_version $VERSION" --define "_release $RELEASE" $SPEC_FILE_DIR/hostagent.spec
 
+cp $SPEC_FILE_DIR/hostagent-rpm-build.sh $TARBALL_EXPANDED_LOCATION/hostagent-rpm-build.sh
 rm -rf $SPEC_FILE_DIR
