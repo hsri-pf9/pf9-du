436ebef4bcd80fc98b53a3eec46f342e27ada896
Paavan Shanbhag <pshanbhag@platform9.net>
2015-04-29 13:55:04 -0700

    Add the host agent extension files for Ubuntu too
    Add the host agent extension files for Ubuntu too. Previous change was
    unknowlingly made to be redhat specific.
    
    Testing done: Built both rpms
    
    Issues addressed: IAAS-2254
    
    Reviewed by: osoriano (https://platform9.atlassian.net/browse/IAAS-2254)

--- bbone/bbslave/support/common-hostagent-defs.mk
+++ bbone/bbslave/support/common-hostagent-defs.mk
@@ -64,4 +64,6 @@ $(VENV_DIR): $(PYTHON_DIR)
 	cd $(VENV_DIR)/bin && ln -s pf9-hostagent pf9-hostd
 	mkdir -p $(HOSTAGENT_TARBALL_SRCDIR)/opt/pf9/support/
 	cp $(SRC_DIR)/support_scripts/common/* $(HOSTAGENT_TARBALL_SRCDIR)/opt/pf9/support
+	mkdir -p $(HOSTAGENT_TARBALL_SRCDIR)/opt/pf9/hostagent/extensions
+	cp $(SRC_DIR)/extensions/* $(HOSTAGENT_TARBALL_SRCDIR)/opt/pf9/hostagent/extensions
 
