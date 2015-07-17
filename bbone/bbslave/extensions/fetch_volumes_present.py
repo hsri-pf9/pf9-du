d8a79d5be7794817633fdc6d29a3fadc3f7597da
Arun Sriraman <sarun@platform9.com>
2015-07-17 16:34:35 -0700

    IAAS-2137 - Fix error when vgs is not present.
    Return an error message when vgs is not present. Handle error conditions.
    Changed sudoers to remove * from vgs command to increase security.

--- bbone/bbslave/extensions/fetch_volumes_present.py
+++ bbone/bbslave/extensions/fetch_volumes_present.py
@@ -6,16 +6,19 @@
 import commands
 import json
 import sys
+import os.path
 
 def get_volumes_list():
     """
     Return the list of volumes present on the host
     """
+    # Check if vgs utility is present. If not present, we cannot run vgs
+    # to find list of volumes. We need lvm2 package to run these commands.
+    if os.path.isfile("/sbin/vgs") is False:
+        return ["Error: No vgs command found."]
     # tail -n+2 removes the header line followed by awk to get only the
     # name of the volumes. Return it as a list.
-    out = commands.getoutput("sudo vgs | tail -n+2 | awk '{print $1}'")
-    if "command not found" in out:
-        return []
+    out = commands.getoutput("sudo /sbin/vgs | tail -n+2 | awk '{print $1}'")
     volumes_list = out.split()
     return volumes_list
 
