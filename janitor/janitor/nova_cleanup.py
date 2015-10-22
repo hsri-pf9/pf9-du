hsri-pf9/pf9-du

commit 2d8013042b11f9a7dbf88b43787f813cf55c71ad

Changes for nova janitor cleanup in vmware to support cluster aggregates
Inorder to support cluster aggregates(IAAS-3785), vmware setup will be
having multiple nova compute nodes per resmgr host. The current janitor
logic deletes any compute node that is not a resgmr host. Changing the
nova cleanup logic to delete compute nodes that are not part of ostackhost
vmw role data

janitor/janitor/nova_cleanup.py
@@ -3,6 +3,9 @@
 # All rights reserved
 #
 
+__author__ = 'Platform9'
+
+import json
 import requests
 import logging
 import utils
@@ -108,6 +111,41 @@ class NovaCleanup(NovaBase):
             if resp.status_code != 204:
                 LOG.error('Skipping hypervisor %s, resp: %d', pf9_id, resp.status_code)
 
+        def is_vmware(resmgr_ids, token):
+            if len(resmgr_ids) == 1 and 'pf9-ostackhost-vmw' in \
+                    utils.get_resmgr_host_roles(self._resmgr_url, token, next(iter(resmgr_ids))):
+                return True
+            return False
+
+        def find_nova_hosts_not_in_resmgr(resmgr_ids, token, project_id):
+            resp = self._nova_request('os-hypervisors/detail', token, project_id)
+
+            if resp.status_code != requests.codes.ok:
+                LOG.error('Unexpected return code: %(code)s when querying hypervisors', dict(code=resp.status_code))
+                return
+
+            nova_data = resp.json()['hypervisors']
+            nova_map = dict()
+            nova_ids = set()
+            vmware = is_vmware(resmgr_ids, token)
+
+            if not vmware:
+                for host in nova_data:
+                    nova_map[host['OS-EXT-PF9-HYP-ATTR:host_id']] = host['id']
+                    nova_ids.add(host['OS-EXT-PF9-HYP-ATTR:host_id'])
+            else:
+                clusters = utils.get_ostackhost_role_data(resmgr_ids,
+                                                          self._resmgr_url,
+                                                          token).split(',')
+                for host in nova_data:
+                    nova_map[host['OS-EXT-PF9-HYP-ATTR:host_id']] = host['id']
+                    # Split the cluster name and verify as Hypervisor hostname is of format
+                    # "domain-c86104(test_cluster)" and role data has just the cluster name
+                    # "test_cluster".
+                    if host['hypervisor_hostname'].split('(')[1].strip(')') not in clusters:
+                        nova_ids.add(host['OS-EXT-PF9-HYP-ATTR:host_id'])
+            return nova_ids.difference(resmgr_ids), nova_map
+
         # 1. Query resmgr hosts
         resp = utils.get_resmgr_hosts(self._resmgr_url, token_id)
 
@@ -118,22 +156,8 @@ class NovaCleanup(NovaBase):
         resmgr_data = resp.json()
         resmgr_ids = set(h['id'] for h in filter(lambda h: h['state'] == 'active', resmgr_data))
 
-        # 2. Query nova hosts
-        resp = self._nova_request('os-hypervisors/detail', token_id, project_id)
-
-        if resp.status_code != requests.codes.ok:
-            LOG.error('Unexpected return code: %(code)s when querying hypervisors', dict(code=resp.status_code))
-            return
-
-        nova_data = resp.json()['hypervisors']
-        nova_map = dict()
-        nova_ids = set()
-        for h in nova_data:
-            nova_map[h['OS-EXT-PF9-HYP-ATTR:host_id']] = h['id']
-            nova_ids.add(h['OS-EXT-PF9-HYP-ATTR:host_id'])
-
-        # 3. Find nova-only hosts
-        nova_only_ids = nova_ids.difference(resmgr_ids)
+        # 2 & 3. Find all nova hosts and nova-only hosts that are not in resmgr
+        nova_only_ids, nova_map = find_nova_hosts_not_in_resmgr(resmgr_ids, token, project_id)
 
         host_to_aggr_map = get_host_to_aggr_map(nova_only_ids)
 

janitor/janitor/utils.py
@@ -71,6 +71,31 @@ def get_resmgr_hosts(resmgr_url, token):
 
     return resp
 
+def get_resmgr_host_roles(resmgr_url, token, resmgr_host):
+    url = '/'.join([resmgr_url, 'v1', 'hosts', resmgr_host])
+    headers = {'X-Auth-Token': token, 'Content-Type': 'application/json'}
+
+    resp = requests.get(url, verify=False, headers=headers)
+
+    if resp.status_code not in (requests.codes.ok, 204):
+        LOG.error('Resource manager query failed: %d', resp.status_code)
+        return []
+    return resp.json()["roles"]
+
+def get_ostackhost_role_data(resmgr_hosts, resmgr_url, token):
+    role = 'pf9-ostackhost-vmw'
+    headers = {'X-Auth-Token': token, 'Content-Type': 'application/json'}
+    clusters = ""
+    for resmgr_host in resmgr_hosts:
+        url = '/'.join([resmgr_url, 'v1', 'hosts', resmgr_host, 'roles', role])
+        resp = requests.get(url, verify=False, headers=headers)
+
+        if resp.status_code not in (requests.codes.ok, 204):
+            LOG.error('Resource manager query failed for host: %s with status: %d',
+                                                    resmgr_host, resp.status_code)
+        else:
+            clusters = clusters + resp.json()['cluster_name']
+    return clusters
 
 def get_keystone_credentials(configfile):
     """

commit a1e71fadfaa3324c5d020369b7bbe4a41fcd54a6

Fix the build - recent janitor changes reference token incorrectly
This was introduced in
https://github.com/platform9/pf9-du/commit/2d8013042b11f9a7dbf88b43787f813cf55c71ad

testing: fixed janitor in broken build in which hypervisor removal
was failing.

janitor/janitor/nova_cleanup.py
@@ -157,7 +157,7 @@ class NovaCleanup(NovaBase):
         resmgr_ids = set(h['id'] for h in filter(lambda h: h['state'] == 'active', resmgr_data))
 
         # 2 & 3. Find all nova hosts and nova-only hosts that are not in resmgr
-        nova_only_ids, nova_map = find_nova_hosts_not_in_resmgr(resmgr_ids, token, project_id)
+        nova_only_ids, nova_map = find_nova_hosts_not_in_resmgr(resmgr_ids, token_id, project_id)
 
         host_to_aggr_map = get_host_to_aggr_map(nova_only_ids)
 