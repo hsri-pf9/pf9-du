bc77ea1934b8a69bfa30d17f81dce09245a174ea
Patrick McIlroy <patrick@platform9.com>
2016-02-06 22:50:02 -0800

    change SKIP_SIGNING to SIGN_PACKAGES

--- sign_packages.sh
+++ sign_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if [ "x${SKIP_SIGNING}" != "x1" ]; then
+if [ "x${SIGN_PACKAGES}" = "x1" ]; then
     expect $(dirname $0)/du.expect $@
 fi
--- sign_packages_deb.sh
+++ sign_packages_deb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if [ "x${SKIP_SIGNING}" != "x1" ]; then
+if [ "x${SIGN_PACKAGES}" = "x1" ]; then
     PATH="$HOME/bin:$PATH" debsigs --sign=origin -v --default-key='Platform9 Systems' $@
 fi
