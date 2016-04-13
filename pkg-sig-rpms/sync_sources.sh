447a2743d6d50bf0682b88924608671f07934bb4
Patrick McIlroy <patrick@platform9.com>
2016-04-13 11:40:37 -0700

    pkg-sig-rpms: conditional handling in subshells are bad

--- pkg-sig-rpms/sync_sources.sh
+++ pkg-sig-rpms/sync_sources.sh
@@ -4,7 +4,7 @@
 
 spec_file=$1
 sources_output_dir=$2
-source_bundle_version=$3
+source_bundle_version=${3:-1}
 
 usage() {
     echo "Usage: $0 <spec file> <source output dir> <version>"
@@ -18,14 +18,17 @@ usage() {
 BUILD_CACHE_ROOT=~/.pf9-du-buildcache
 BUNDLE_DIR="$spec_file-$source_bundle_version"
 
+[ ! -d $BUILD_CACHE_ROOT ] && mkdir -p $BUILD_CACHE_ROOT
+[ ! -d $sources_output_dir ] && mkdir -p $sources_output_dir
+
 if [ -d $BUILD_CACHE_ROOT/$BUNDLE_DIR ]; then
     cp -R $BUILD_CACHE_ROOT/$BUNDLE_DIR/* $sources_output_dir
 else
     mkdir -p $BUILD_CACHE_ROOT/$BUNDLE_DIR/
-    spectool -g -S -C $sources_output_dir $spec_file || (
+    if ! spectool -g -S -C $sources_output_dir $spec_file; then
         echo "Could not retrieve all source files, eliminating build cache" >&2
-        rm -rf $BUILD_CACHE_ROOT/$spec_file
+        rm -rf $BUILD_CACHE_ROOT/$BUNDLE_DIR
         exit 1
-    )
+    fi
     cp -R $sources_output_dir/* $BUILD_CACHE_ROOT/$BUNDLE_DIR/
 fi
