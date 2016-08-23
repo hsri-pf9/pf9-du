31995467202643b2f9aa55644c5ff97b707293ed
Vann Teves <vteves@platform9.com>
2016-08-23 02:25:12 +0000

    DB Migration script for pf9-cindervolume
    To enable support for UI-based cinder driver configuration,
    we are introducing driver specific roles. Each role will have its
    own set of customizable settings.
    
    pf9-cindervolume will now be renamed to pf9-cindervolume-base and
    each driver will have a specific role (e.g. pf9-cindervolume-lvm for LVM).
    
    As for other drivers we currently don't officially support,
    we authorize the pf9-cindervolume-other role. The after-install script
    takes care of moving conf files from /opt/pf9/etc/pf9-cindervolume/conf.d
    to /opt/pf9/etc/pf9-cindervolume-base/conf.d

---
+++ resmgr/resmgr/alembic/versions/9_migrate_pf9_cindervolume.py
@@ -0,0 +1,55 @@
+"""
+Migrate pf9-cindervolume role
+Revision ID: 9
+Revises: 8
+Create Date: 2016-08-17 19:00:00.567532
+"""
+
+# revision identifiers, used by Alembic.
+revision = '9'
+down_revision = '8'
+
+from alembic import op
+import json
+import logging
+log = logging.getLogger(__name__)
+
+def upgrade():
+    conn = op.get_bind()
+    log.info("Starting upgrade of pf9-cindervolume role")
+    stmt = 'select id, hostname, role_settings from hosts'
+    hosts = conn.execute(stmt)
+    host_list = hosts.fetchall()
+    for host in host_list:
+      host_id  = host[0]
+      hostname = host[1]
+      role_settings = json.loads(host[2])
+
+      driver_role_mapping = {
+          "cinder.volume.drivers.lvm.LVMISCSIDriver": "pf9-cindervolume-lvm",
+          "cinder.volume.drivers.netapp.common.NetAppDriver": "pf9-cinderovlume-netapp",
+          "cinder.volume.drivers.qnap.QnapISCSIDriver":  "pf9-cindervolume-qnap",
+          "cinder.volume.drivers.solidfire.SolidFireDriver": "pf9-cindervolume-solidfire"
+      }
+
+      if 'pf9-cindervolume' in role_settings and 'pf9-cindervolume-base' not in role_settings:
+          log.info("Host {0} ({1}) has pf9-cindervolume role and no pf9-cindervolume-base role".format(
+                   host_id, hostname))
+          log.info("Current role_settings:\n{0}".format(json.dumps(role_settings)))
+          current_settings = role_settings.pop("pf9-cindervolume")
+          current_driver = current_settings["volume_driver"]
+
+          # auth with the pf9-cindervolume-base role by default
+          role_settings["pf9-cindervolume-base"] = {}
+          if current_driver in driver_role_mapping:
+              role_settings[current_driver] = current_settings
+          else:
+              role_settings['pf9-cindervolume-other'] = current_settings
+          stmt = 'update hosts set role_settings=\'{0}\' where id="{1}"'.format(
+            json.dumps(role_settings), host_id)
+          log.info("New role settings:\n{0}".format(json.dumps(role_settings)))
+          conn.execute(stmt)
+      elif 'pf9-cindervolume-base' in role_settings:
+          log.info("Host {0} ({1}) is already up-to-date".format(host_id, hostname))
+def downgrade():
+    pass
