8d983d37994ed78d24cf4780ac71e91238848124
Vann Teves <vteves@platform9.com>
2016-09-16 22:50:03 +0000

    Fix pf9-cindervolume upgrade script part 2
    To upgrade a role, the upgrade_hosts.py script loops over a host's
    list of currently authorized roles and sends a put request for the same
    rolename. This action triggers a resmgr update to the role.
    
    Since the new roles do not exist yet, the upgrade script
    won't be able to do so for our newly renamed and added roles.
    
    The workaround is to add dummy roles with the equivalent rolenames.
    
    Testing:
    Passed an upgrade with VMware. The appliance initially had the
    pf9-cindervolume role and after upgrade had both pf9-cindervolume-base
    and pf9-cindervolume-lvm (since it was auth with the lvm driver).
    
    OK with KVM (resmgr returned the correct roles and DB is as expected).
    However, actual upgrade failed due to some sidekick issue.

--- resmgr/resmgr/alembic/versions/9_migrate_pf9_cindervolume.py
+++ resmgr/resmgr/alembic/versions/9_migrate_pf9_cindervolume.py
@@ -14,12 +14,29 @@ import json
 import logging
 log = logging.getLogger(__name__)
 
+
+conn = op.get_bind()
+
+def get_role_id(rolename):
+    stmt = 'select id from roles where rolename="%s" and active="1"' % rolename
+    res  = conn.execute(stmt).fetchone()
+    if res != None:
+      return res['id']
+    else:
+      return None
+
+def insert_dummy_role(rolename):
+    log.info("Adding a dummy %s role" % rolename)
+    version = "2.2.0-000"
+    stmt = 'insert into roles values (%s, %s, %s,"Block Storage", "Temp role", "{}", 1, "{}", "{}")'
+    res  = conn.execute(stmt, rolename + '-temp', rolename, version)
+
 def upgrade():
-    conn = op.get_bind()
     log.info("Starting upgrade of pf9-cindervolume role")
     stmt = 'select id, hostname, role_settings from hosts'
     hosts = conn.execute(stmt)
     host_list = hosts.fetchall()
+
     for host in host_list:
       host_id  = host[0]
       hostname = host[1]
@@ -41,18 +58,42 @@ def upgrade():
 
           # auth with the pf9-cindervolume-base role by default
           role_settings["pf9-cindervolume-base"] = {}
+          driver_role = driver_role_mapping[current_driver]
           if current_driver in driver_role_mapping:
-              role_settings[driver_role_mapping[current_driver]] = current_settings
+              role_settings[driver_role] = current_settings
           else:
               role_settings['pf9-cindervolume-other'] = current_settings
           stmt = 'update hosts set role_settings=%s where id=%s'
           log.info("New role settings:\n{0}".format(json.dumps(role_settings)))
           conn.execute(stmt, json.dumps(role_settings), host_id)
 
-          # deactivate pf9-cindervolume
-          log.info("Deactivating pf9-cindervolume role")
-          stmt = 'update roles set active=0 where rolename="pf9-cindervolume"'
-          conn.execute(stmt)
+          # Delete pf9-cindervolume
+          role_id = get_role_id("pf9-cindervolume")
+          log.info(role_id)
+          if role_id != None:
+              # Add a dummy pf9-cindervolume-* first
+              insert_dummy_role("pf9-cindervolume-base")
+              insert_dummy_role(driver_role)
+
+              log.info("Deleting pf9-cindervolume host_role_map association")
+              stmt = 'delete from host_role_map where rolename="{0}"'.format(role_id)
+              conn.execute(stmt)
+
+              log.info("Deleting pf9-cindervolume role")
+              stmt = 'delete from roles where rolename="pf9-cindervolume"'
+              conn.execute(stmt)
+
+          # Associate host and roles
+          # cindervolume-base
+          stmt = 'insert into host_role_map values (%s, %s)'
+          cindervolume_base_id = get_role_id("pf9-cindervolume-base")
+          conn.execute(stmt, host_id, cindervolume_base_id)
+
+          # + custom cindervolume driver role
+          stmt = 'insert into host_role_map values (%s, %s)'
+          role_id = get_role_id(driver_role)
+          conn.execute(stmt, host_id, role_id)
+
       elif 'pf9-cindervolume-base' in role_settings:
           log.info("Host {0} ({1}) is already up-to-date".format(host_id, hostname))
 def downgrade():
