cc58f5e1ead7c479c764cf5539b8272554b1e826
Vann Teves <vteves@platform9.com>
2016-09-21 22:59:15 +0000

    IAAS-6328: Only associate cinder roles to hosts with the cinder role association
    Testing:
            Ran the DB migration script +
            Tried a case where another host had a custom setting for
            cinder but with the cinder role not applied--verified that
            the upgrade script did not insert a role association.
    
            Also verified that role association and customizable settings
            are set correct for multiple cinder hosts (where the cinder
            role association was set).

--- resmgr/resmgr/alembic/versions/9_migrate_pf9_cindervolume.py
+++ resmgr/resmgr/alembic/versions/9_migrate_pf9_cindervolume.py
@@ -31,12 +31,34 @@ def insert_dummy_role(rolename):
     stmt = 'insert into roles values (%s, %s, %s,"Block Storage", "Temp role", "{}", 1, "{}", "{}")'
     res  = conn.execute(stmt, rolename + '-temp', rolename, version)
 
+def insert_dummy_roles():
+    dummy_roles = ["pf9-cindervolume-base", "pf9-cindervolume-lvm",
+                   "pf9-cindervolume-netapp", "pf9-cindervolume-qnap",
+                   "pf9-cindervolume-solidfire", "pf9-cindervolume-other"]
+    for role in dummy_roles:
+        insert_dummy_role(role)
+
 def upgrade():
     log.info("Starting upgrade of pf9-cindervolume role")
     stmt = 'select id, hostname, role_settings from hosts'
     hosts = conn.execute(stmt)
     host_list = hosts.fetchall()
 
+    old_cinder_role_id = get_role_id("pf9-cindervolume")
+    if old_cinder_role_id == None:
+        log.info("No active pf9-cindervolume role. Exiting")
+        return
+
+    stmt = 'select res_id, rolename from host_role_map where rolename=%s'
+    query = conn.execute(stmt, old_cinder_role_id)
+    role_map_list = query.fetchall()
+
+    # Insert dummy roles so resmgr can trigger a PUT request
+    # during upgrades
+    insert_dummy_roles()
+    cindervolume_base_id = get_role_id("pf9-cindervolume-base")
+
+    # Update customizable settings
     for host in host_list:
       host_id  = host[0]
       hostname = host[1]
@@ -58,43 +80,40 @@ def upgrade():
 
           # auth with the pf9-cindervolume-base role by default
           role_settings["pf9-cindervolume-base"] = {}
-          driver_role = driver_role_mapping[current_driver]
+          driver_role = "pf9-cindervolume-other"
           if current_driver in driver_role_mapping:
+              driver_role = driver_role_mapping[current_driver]
               role_settings[driver_role] = current_settings
           else:
-              role_settings['pf9-cindervolume-other'] = current_settings
+              role_settings[driver_role] = current_settings
           stmt = 'update hosts set role_settings=%s where id=%s'
           log.info("New role settings:\n{0}".format(json.dumps(role_settings)))
           conn.execute(stmt, json.dumps(role_settings), host_id)
 
-          # Delete pf9-cindervolume
-          role_id = get_role_id("pf9-cindervolume")
-          log.info(role_id)
-          if role_id != None:
-              # Add a dummy pf9-cindervolume-* first
-              insert_dummy_role("pf9-cindervolume-base")
-              insert_dummy_role(driver_role)
+          for res_id, role_id in role_map_list:
+              if res_id != host_id:
+                  continue
+              else:
+                  log.info("Updating cindervolume host_role_map association for {0}".format(res_id))
+                  stmt = 'delete from host_role_map where rolename=%s and res_id=%s'
+                  conn.execute(stmt, role_id, res_id)
 
-              log.info("Deleting pf9-cindervolume host_role_map association")
-              stmt = 'delete from host_role_map where rolename="{0}"'.format(role_id)
-              conn.execute(stmt)
+                  # Associate host and roles if an existing one appears in the host_role_map table
+                  stmt = 'insert into host_role_map values (%s, %s)'
+                  conn.execute(stmt, res_id , cindervolume_base_id)
 
-              log.info("Deleting pf9-cindervolume role")
-              stmt = 'delete from roles where rolename="pf9-cindervolume"'
-              conn.execute(stmt)
-
-          # Associate host and roles
-          # cindervolume-base
-          stmt = 'insert into host_role_map values (%s, %s)'
-          cindervolume_base_id = get_role_id("pf9-cindervolume-base")
-          conn.execute(stmt, host_id, cindervolume_base_id)
-
-          # + custom cindervolume driver role
-          stmt = 'insert into host_role_map values (%s, %s)'
-          role_id = get_role_id(driver_role)
-          conn.execute(stmt, host_id, role_id)
+                  # + custom cindervolume driver role
+                  stmt = 'insert into host_role_map values (%s, %s)'
+                  role_id = get_role_id(driver_role)
+                  conn.execute(stmt, res_id, role_id)
 
       elif 'pf9-cindervolume-base' in role_settings:
           log.info("Host {0} ({1}) is already up-to-date".format(host_id, hostname))
+
+    # Delete old pf9-cindervolume role
+    log.info("Deleting pf9-cindervolume role")
+    stmt = 'delete from roles where rolename="pf9-cindervolume"'
+    conn.execute(stmt)
+
 def downgrade():
     pass
