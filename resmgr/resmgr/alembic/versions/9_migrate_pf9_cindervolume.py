fc460f65894171425cdf8a337059de1aa8e64630
Vann Teves <vteves@platform9.com>
2016-10-02 02:10:36 +0000

    Improve cindervolume upgrade script
    1. Update all hosts with the pf9-cindervolume role regardless of whether the
    role is active or inactive.
    2. Make upgrade script idempotent by making sure that we only insert
    dummy roles when they don't exist.
    
    Testing:
            Made sure that script doesn't crash when hosts are authorized with
            inactive cindervolume roles.
            Ran the script after the cindervolume roles were upgraded to
            verify that the script is idempotent.

--- resmgr/resmgr/alembic/versions/9_migrate_pf9_cindervolume.py
+++ resmgr/resmgr/alembic/versions/9_migrate_pf9_cindervolume.py
@@ -19,24 +19,21 @@ conn = op.get_bind()
 
 def get_role_id(rolename):
     stmt = 'select id from roles where rolename="%s" and active="1"' % rolename
-    res  = conn.execute(stmt).fetchone()
+    res = conn.execute(stmt).fetchone()
     if res != None:
-      return res['id']
+        log.info("Got active role {0} ({1})".format(rolename, res['id']))
+        return res['id']
     else:
-      return None
+        return None
 
 def insert_dummy_role(rolename):
-    log.info("Adding a dummy %s role" % rolename)
-    version = "2.2.0-000"
-    stmt = 'insert into roles values (%s, %s, %s,"Block Storage", "Temp role", "{}", 1, "{}", "{}")'
-    res  = conn.execute(stmt, rolename + '-temp', rolename, version)
-
-def insert_dummy_roles():
-    dummy_roles = ["pf9-cindervolume-base", "pf9-cindervolume-lvm",
-                   "pf9-cindervolume-netapp", "pf9-cindervolume-qnap",
-                   "pf9-cindervolume-solidfire", "pf9-cindervolume-other"]
-    for role in dummy_roles:
-        insert_dummy_role(role)
+    if get_role_id(rolename) == None:
+        log.info("Adding a dummy %s role" % rolename)
+        version = "2.2.0-000"
+        stmt = 'insert into roles values (%s, %s, %s,"Block Storage", "Temp role", "{}", 1, "{}", "{}")'
+        res  = conn.execute(stmt, rolename + '-temp', rolename, version)
+    else:
+        log.info("Dummy role {0} already present.".format(rolename))
 
 def upgrade():
     log.info("Starting upgrade of pf9-cindervolume role")
@@ -44,20 +41,16 @@ def upgrade():
     hosts = conn.execute(stmt)
     host_list = hosts.fetchall()
 
-    old_cinder_role_id = get_role_id("pf9-cindervolume")
-    if old_cinder_role_id == None:
-        log.info("No active pf9-cindervolume role. Exiting")
-        return
-
-    stmt = 'select res_id, rolename from host_role_map where rolename=%s'
-    query = conn.execute(stmt, old_cinder_role_id)
+    stmt = 'select host_role_map.res_id, host_role_map.rolename from host_role_map\
+            join\
+            roles where host_role_map.rolename=roles.id\
+            and roles.rolename="pf9-cindervolume"'
+    query = conn.execute(stmt)
     role_map_list = query.fetchall()
+    log.info("Hosts authorized with active and inactive pf9-cindervolume roles: {0}".format(
+             role_map_list))
 
-    # Insert dummy roles so resmgr can trigger a PUT request
-    # during upgrades
-    insert_dummy_roles()
-    cindervolume_base_id = get_role_id("pf9-cindervolume-base")
-
+    cinder_base_role = 'pf9-cindervolume-base'
     # Update customizable settings
     for host in host_list:
       host_id  = host[0]
@@ -71,7 +64,7 @@ def upgrade():
           "cinder.volume.drivers.solidfire.SolidFireDriver": "pf9-cindervolume-solidfire"
       }
 
-      if 'pf9-cindervolume' in role_settings and 'pf9-cindervolume-base' not in role_settings:
+      if 'pf9-cindervolume' in role_settings and cinder_base_role not in role_settings:
           log.info("Host {0} ({1}) has pf9-cindervolume role and no pf9-cindervolume-base role".format(
                    host_id, hostname))
           log.info("Current role_settings:\n{0}".format(json.dumps(role_settings)))
@@ -79,7 +72,7 @@ def upgrade():
           current_driver = current_settings["volume_driver"]
 
           # auth with the pf9-cindervolume-base role by default
-          role_settings["pf9-cindervolume-base"] = {}
+          role_settings[cinder_base_role] = {}
           driver_role = "pf9-cindervolume-other"
           if current_driver in driver_role_mapping:
               driver_role = driver_role_mapping[current_driver]
@@ -89,6 +82,12 @@ def upgrade():
           stmt = 'update hosts set role_settings=%s where id=%s'
           log.info("New role settings:\n{0}".format(json.dumps(role_settings)))
           conn.execute(stmt, json.dumps(role_settings), host_id)
+
+          # Insert dummy roles so resmgr can trigger a PUT request
+          # during upgrades
+          insert_dummy_role(cinder_base_role)
+          cindervolume_base_id = get_role_id(cinder_base_role)
+          insert_dummy_role(driver_role)
 
           for res_id, role_id in role_map_list:
               if res_id != host_id:
