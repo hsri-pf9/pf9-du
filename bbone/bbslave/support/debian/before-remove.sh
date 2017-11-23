fe003fedf1d9a940d981a77f616c441b06a6b006
Paavan Shanbhag <pshanbhag@platform9.net>
2017-11-22 16:09:36 -0800

    Fix the hostagent uninstall for both Ubuntu versions
    Ubuntu14 is sysVinit based. Ubuntu16 is systemd based. Both have a different
    sequence of steps when deregistering the hostagent service with init or systemd.
    This change is based on the OS specific requirement.
    
    Testing done: Installed hostagent and performed uninstall on both Ubuntu 14 and
    Ubuntu 16.
    
    Issues: PMK-1006
    
    Review: https://rbcommons.com/s/platform9/r/7921/

--- bbone/bbslave/support/debian/before-remove.sh
+++ bbone/bbslave/support/debian/before-remove.sh
@@ -10,10 +10,14 @@ new_version=$2
 if [ "$script_step" = "remove" ]; then
     . /opt/pf9/pf9-service-functions.sh
     pf9_service_stop pf9-hostagent > /dev/null 2>&1
-    pf9_disable_service_on_boot pf9-hostagent > /dev/null 2>&1
-    pf9_remove_service_files pf9-hostagent
+    if is_ubuntu_14; then
+        pf9_remove_service_files pf9-hostagent
+        pf9_disable_service_on_boot pf9-hostagent > /dev/null 2>&1
+    fi
 
     if is_ubuntu_16; then
+        pf9_disable_service_on_boot pf9-hostagent > /dev/null 2>&1
+        pf9_remove_service_files pf9-hostagent
         LINKED_FILES="apt_inst.so  apt_pkg.so"
         for ln_file in $LINKED_FILES
         do
