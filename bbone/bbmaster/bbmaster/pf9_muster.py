b728486f02ad02fdcaf9eb425f0b1ae2dba027fb
Paavan Shanbhag <pshanbhag@platform9.net>
2018-04-13 14:08:55 -0700

    Avoid muster being pushed for ISV case
    Muster is not necessary to be setup in case of ISV. In order to prevent this
    being pushed to hosts in ISV case, change the bbmaster logic to ignore muster
    app if there is an env var set as "ISV=True"
    
    The ISV setup script will pass this variable in. In other scenarios, this
    variable will not be present (or will be set to False)
    
    Review: https://rbcommons.com/s/platform9/r/8991/

--- bbone/bbmaster/bbmaster/pf9_muster.py
+++ bbone/bbmaster/bbmaster/pf9_muster.py
@@ -3,6 +3,23 @@
 
 # TODO: Implement interface so that new apps can follow same structure by
 #       implementing it
+import os
+from ConfigParser import ConfigParser
+from bbcommon import constants
+
+bbm_config = ConfigParser()
+bbm_conf = os.environ.get('BBMASTER_CONFIG_FILE',
+                          constants.BBMASTER_CONFIG_FILE)
+bbm_config.read(bbm_conf)
+
+
+@property
+def is_isv():
+    """
+    Whether the service is running in an ISV setup or not
+    """
+    return bbm_config.has_option('bbmaster', 'deploy_env') and \
+           bbm_config.get('bbmaster', 'deploy_env').lower() == 'isv'
 
 
 def insert_app_config(desired_apps, muster_cfg, host_state=None):
@@ -11,8 +28,9 @@ def insert_app_config(desired_apps, muster_cfg, host_state=None):
     compatibility across firmware apps.
     Inserts the specified pf9-muster configuration into the supplied dictionary.
     """
-    if type(desired_apps) is dict:
-        desired_apps['pf9-muster'] = muster_cfg
+    if is_isv:
+        if type(desired_apps) is dict:
+            desired_apps['pf9-muster'] = muster_cfg
     return desired_apps
 
 
