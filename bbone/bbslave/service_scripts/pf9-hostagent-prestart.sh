a35152db719b9c5f99cceae1c31d5faf9687fe5e
Hongwei Liu <hongwei@platform9.com>
2019-03-26 13:26:20 -0700

    extension to get mounted nfs file system

--- bbone/bbslave/service_scripts/pf9-hostagent-prestart.sh
+++ bbone/bbslave/service_scripts/pf9-hostagent-prestart.sh
@@ -3,6 +3,10 @@
 mkdir -p /tmp/python-eggs
 chown pf9:pf9group /tmp/python-eggs
 
+# create cache folder and set owner before hostagent runs
+mkdir -p /opt/pf9/cache
+chown -R pf9:pf9group /opt/pf9/cache
+
 . /etc/os-release
 
 if [[ "$ID" == "ubuntu" && "$VERSION_ID" == "16.04" ]]
