0a6b1c73754b3913132fe36fa44b9f0da9faa508
Paavan Shanbhag <pshanbhag@platform9.com>
2019-06-07 14:13:34 -0700

    Revert "Patch muster deb files as part of hostagent upgrade"
    This reverts commit d440537b1687b0e50ee26f87576d82df56cd0ce8.
    
    Issues: CORE-548
    
    Review: https://rbcommons.com/s/platform9/r/10506/

--- bbone/bbslave/support/debian/muster_IAAS-8635_patch/pf9-muster-before-remove.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Used only by the deb installer. For the corresponding rpm actions, see pf9-muster.spec's %preun section.
-
-service pf9-muster stop >/dev/null 2>&1
-rm -rf /var/run/pf9-muster
-
