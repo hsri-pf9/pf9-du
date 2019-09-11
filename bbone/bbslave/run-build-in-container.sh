a4b1c3dc600646682757b7386d5d1902670e4803
Sean Demura <sean@platform9.com>
2019-09-11 16:36:18 -0400

    jq in container

--- bbone/bbslave/run-build-in-container.sh
+++ bbone/bbslave/run-build-in-container.sh
@@ -9,5 +9,8 @@ cp /root/gpg-shim /home/pf9/bin/gpg
 cp -R /dockstage/.{gnupg,gpgpass,rpmmacros} /home/pf9
 chown -R pf9:pf9 /home/pf9 /buildroot
 
+wget -O /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod +x /usr/bin/jq
+
 sudo -u pf9 make -C /buildroot/pf9-du/bbone/bbslave/support/redhat hostagent-tarball \
             TARGET_DISTRO=redhat
