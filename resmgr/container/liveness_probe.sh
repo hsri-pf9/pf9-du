009b16acc1ba10dd8889ffc38f55d713e1714880
nehatomar12 <nehatomar12494@gmail.com>
2023-12-08 17:43:59 +0530

    Adding timeout of 60 sec for resmgr api

--- resmgr/container/liveness_probe.sh
+++ resmgr/container/liveness_probe.sh
@@ -5,7 +5,7 @@ exec >> /var/log/pf9/bbmaster.log 2>&1
 echo "Starting health check at $(date)"
 
 # Perform the health check using curl and capture the HTTP status code
-http_status=$(curl -sS -o /dev/null -w "%{http_code}" --max-time 90 http://localhost:8082/v1/hosts)
+http_status=$(curl -sS -o /dev/null -w "%{http_code}" --max-time 60 http://localhost:8082/v1/hosts)
 
 # Log the HTTP status code
 echo "HTTP status code: $http_status"
